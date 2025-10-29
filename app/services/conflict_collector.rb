3c6469dc67c4d1198adc553b18e6363db9351f8b
Josiah <josiah.pilon@gmail.com>
2025-10-29 11:53:30 -0700

    adds conflict collector service to collect any conflicts

---
+++ app/services/conflict_collector.rb
@@ -0,0 +1,31 @@
+class ConflictCollector
+  attr_reader :prospective_matter, :detectors
+
+  def self.search(prospective_matter)
+    instance = new(prospective_matter)
+    instance.search
+  end
+
+  def initialize(prospective_matter)
+    @prospective_matter = prospective_matter
+    @detectors = load_detectors
+  end
+
+  def search
+    detectors.map do |dector|
+      former_legal_matters.filter_map do |former_legal_matter|
+        dector.detect(former_legal_matter, prospective_matter)
+      end
+    end
+  end
+
+  private
+
+  def load_detectors
+    ConflictDetectors::ConflictDetector.descendants
+  end
+
+  def former_legal_matters
+    LegalMatter.all
+  end
+end
